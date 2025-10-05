746691ec8ec4fde9ab902436b89320007e3bddfc
turiddu25 <colin.nardo@gmail.com>
2025-10-05 01:07:50 +0100

    sanity cli

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'x9t2iascavp2k8pxr163et4l',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
